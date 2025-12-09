657900c3e521180fbb4851b3741efe93fae1e2a4
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2025-12-09 15:44:07 -0500

    Notion tool removed

--- src/utils/tool_registry.py
+++ src/utils/tool_registry.py
@@ -1,5 +1,5 @@
 from ..tools.docx_read_tool_langgraph import docx_read_tool_langgraph
-from ..tools.notion_tool_langgraph import notion_create_page_langgraph
+# from ..tools.notion_tool_langgraph import notion_create_page_langgraph
 from langchain_tavily import TavilySearch
 
 tavily_tool = TavilySearch(max_results=2)
@@ -9,4 +9,4 @@ def get_tools_for_agent(agent_key: str):
         return [tavily_tool] if tavily_tool else []
     if agent_key == 'screenwriter':
         return [docx_read_tool_langgraph]
-    return [] 
+    return []
